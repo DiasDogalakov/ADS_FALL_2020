d7d18459f7da4306b21672dd400fa5e289833a1a
DiasDogalakov <60038731+DiasDogalakov@users.noreply.github.com>
2020-09-28 11:24:53 +0600

    Create H.cpp

---
+++ LAB 3/H.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+ 
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cin >> n;
+    int ans = ceil(log2(n));
+    cout << ans;
+    return 0;
+}
