a0b0cc04a7cd195cbc55d0c8137d3d3fe60cc65b
DiasDogalakov <60038731+DiasDogalakov@users.noreply.github.com>
2020-10-28 19:38:28 +0600

    Create bubble_sort.cpp

---
+++ sort/bubble_sort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+ 
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for(int j = 0; j < n-1; j++)
+    {
+        for(int i = 0; i < n-1; i++)
+        {
+            if(arr[i] > arr[i+1])
+            {
+                swap(arr[i], arr[i+1]);
+            }
+        }
+    }
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
