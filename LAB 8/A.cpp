4622dcc9957fbfd9fcbb1fba674020ba0117e5d9
Dogalakov Dias <60038731+DiasDogalakov@users.noreply.github.com>
2020-12-07 04:23:45 +0600

    Create A.cpp

---
+++ LAB 8/A.cpp
@@ -0,0 +1,50 @@
+#include <iostream> //ok
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <stack>
+#include <fstream>
+#define ll long long
+
+using namespace std;
+ 
+int main()
+{
+  int x, i, j;
+  cin >> x;
+  int count = 0;
+  int **a = new int*[x];
+  int *b = new int[x];
+  for (int i = 0; i < x; i++)
+  {
+    a[i] = new int[x];
+  }
+
+  for (int i = 0; i < x; i++)
+  {
+    for (int j = 0; j < x; j++)
+    {
+      cin >> a[i][j];
+    }
+  }
+
+  for (int i = 0; i < x; i++)
+  {
+    cin >> b[i];
+  }
+
+  for (i = 0; i < x; i++)
+  {
+    for (j = i + 1; j < x; j++)
+    {
+      if (a[i][j] == 1)
+      {
+        if (b[i] != b[j])
+          count++;
+      }
+    }
+  }
+  cout << count;
+}
